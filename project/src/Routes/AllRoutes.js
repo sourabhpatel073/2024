426cb95b2231d09af15b14e49a4e9ab69b54edab
Sourabh Patel <sourabhpatel073@gmail.com>
2024-01-30 23:35:42 +0530

    day2

---
+++ project/src/Routes/AllRoutes.js
@@ -0,0 +1,14 @@
+import {Routes,Route} from "react-router-dom"
+import Home from "../Components/Home"
+
+function AllRoutes() {
+  return (
+    <div>
+      <Routes>
+<Route path="/" element={<Home/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default AllRoutes
