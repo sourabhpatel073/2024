537a3726c46bf7f42d43dd0b60f3a635f05a3bad
Sourabh Patel <sourabhpatel073@gmail.com>
2024-03-10 00:14:21 +0530

    comp

---
+++ project/src/Components/ArchiveIcon.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Icon } from "@chakra-ui/react";
+import { MdArchive } from "react-icons/md";
+
+const ArchiveIcon = (props) => {
+  return <Icon as={MdArchive} {...props} />;
+};
+
+export default ArchiveIcon;
