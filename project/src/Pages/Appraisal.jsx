d65c960f3987999448901704183b4e301cce00b3
Sourabh Patel <112695694+sourabhpatel073@users.noreply.github.com>
2025-05-24 23:55:27 +0530

    Update Appraisal.jsx

--- project/src/Pages/Appraisal.jsx
+++ project/src/Pages/Appraisal.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
 function Appraisal() {
+  const [count , setcount] = useState(0);
+  function Inc (){
+    setCount(count++)
+  }
   return (
     <div>
       Apprarisals
