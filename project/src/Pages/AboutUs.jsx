17ed16cda355eb282731171476464051a2087b92
Sourabh Patel <112695694+sourabhpatel073@users.noreply.github.com>
2025-05-29 22:34:11 +0530

    Update AboutUs.jsx

--- project/src/Pages/AboutUs.jsx
+++ project/src/Pages/AboutUs.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-
+import {useState} from 'react'
 function AboutUs() {
+  const [ Count , setCount ] = useState (0)
   return (
     <div>
       AboutUs this is the dummy code 
