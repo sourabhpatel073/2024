50031db12b6b4ea43851e4ca546c645dd22a3d90
Sourabh Patel <112695694+sourabhpatel073@users.noreply.github.com>
2025-05-30 20:09:56 +0530

    Update AppSetup.jsx

--- project/src/Pages/AppSetup.jsx
+++ project/src/Pages/AppSetup.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
-
+import {useEffect} from 'react'
 function AppSetup() {
+  function fetchData (){
+    //get data
+  }
+
+  useEfect(()=>{
+    fetchData()
+  },[])
   return (
     <div>
       appsetup
